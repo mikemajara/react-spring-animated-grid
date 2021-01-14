41ab3e8c569062aadec465c4e3ef0c44645e1c26
mikemajara <fmiguel05@gmail.com>
2021-01-14 20:30:44 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -50,67 +50,98 @@ export default function App() {
     { key: "8", bgcolor: "yellow", measure: useMeasure() },
     { key: "9", bgcolor: "magenta", measure: useMeasure() },
     { key: "10", bgcolor: "cyan", measure: useMeasure() },
-    { key: "11", bgcolor: "yellow", measure: useMeasure() },
-    { key: "12", bgcolor: "magenta", measure: useMeasure() },
-    { key: "13", bgcolor: "cyan", measure: useMeasure() },
-    { key: "14", bgcolor: "yellow", measure: useMeasure() },
-    { key: "15", bgcolor: "magenta", measure: useMeasure() },
-    { key: "16", bgcolor: "cyan", measure: useMeasure() },
-    { key: "17", bgcolor: "yellow", measure: useMeasure() },
-    { key: "18", bgcolor: "magenta", measure: useMeasure() },
-    { key: "19", bgcolor: "cyan", measure: useMeasure() },
-    { key: "20", bgcolor: "yellow", measure: useMeasure() },
-    { key: "21", bgcolor: "magenta", measure: useMeasure() },
-    { key: "22", bgcolor: "cyan", measure: useMeasure() },
-    { key: "23", bgcolor: "yellow", measure: useMeasure() },
-    { key: "24", bgcolor: "magenta", measure: useMeasure() },
-    { key: "25", bgcolor: "cyan", measure: useMeasure() },
-    { key: "26", bgcolor: "yellow", measure: useMeasure() },
-    { key: "27", bgcolor: "magenta", measure: useMeasure() },
-    { key: "28", bgcolor: "cyan", measure: useMeasure() },
-    { key: "29", bgcolor: "yellow", measure: useMeasure() },
-    { key: "30", bgcolor: "cyan", measure: useMeasure() }, 
-    { key: "31", bgcolor: "yellow", measure: useMeasure() }, 
-    { key: "32", bgcolor: "magenta", measure: useMeasure() }, 
-    { key: "33", bgcolor: "cyan", measure: useMeasure() }, 
-    { key: "34", bgcolor: "yellow", measure: useMeasure() }, 
-    { key: "35", bgcolor: "magenta", measure: useMeasure() }, 
-    { key: "36", bgcolor: "cyan", measure: useMeasure() },
-    { key: "37", bgcolor: "yellow", measure: useMeasure() },
-    { key: "38", bgcolor: "magenta", measure: useMeasure() },
-    { key: "39", bgcolor: "cyan", measure: useMeasure() },
-    { key: "40", bgcolor: "yellow", measure: useMeasure() },
-    { key: "41", bgcolor: "magenta", measure: useMeasure() },
-    { key: "42", bgcolor: "cyan", measure: useMeasure() },
-    { key: "43", bgcolor: "yellow", measure: useMeasure() },
-    { key: "44", bgcolor: "magenta", measure: useMeasure() },
-    { key: "45", bgcolor: "cyan", measure: useMeasure() },
-    { key: "46", bgcolor: "yellow", measure: useMeasure() },
-    { key: "47", bgcolor: "magenta", measure: useMeasure() },
-    { key: "48", bgcolor: "cyan", measure: useMeasure() },
-    { key: "49", bgcolor: "yellow", measure: useMeasure() },
-    { key: "50", bgcolor: "magenta", measure: useMeasure() },
-    { key: "51", bgcolor: "cyan", measure: useMeasure() },
-    { key: "52", bgcolor: "yellow", measure: useMeasure() },
-    { key: "53", bgcolor: "magenta", measure: useMeasure() },
-    { key: "54", bgcolor: "cyan", measure: useMeasure() },
-    { key: "55", bgcolor: "yellow", measure: useMeasure() },
-    { key: "56", bgcolor: "magenta", measure: useMeasure() },
-    { key: "57", bgcolor: "cyan", measure: useMeasure() },
-    { key: "58", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "11", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "12", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "13", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "14", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "15", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "16", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "17", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "18", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "19", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "20", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "21", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "22", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "23", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "24", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "25", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "26", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "27", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "28", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "29", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "30", bgcolor: "cyan", measure: useMeasure() }, 
+    // { key: "31", bgcolor: "yellow", measure: useMeasure() }, 
+    // { key: "32", bgcolor: "magenta", measure: useMeasure() }, 
+    // { key: "33", bgcolor: "cyan", measure: useMeasure() }, 
+    // { key: "34", bgcolor: "yellow", measure: useMeasure() }, 
+    // { key: "35", bgcolor: "magenta", measure: useMeasure() }, 
+    // { key: "36", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "37", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "38", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "39", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "40", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "41", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "42", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "43", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "44", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "45", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "46", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "47", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "48", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "49", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "50", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "51", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "52", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "53", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "54", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "55", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "56", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "57", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "58", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "59", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "60", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "61", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "62", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "63", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "64", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "65", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "66", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "67", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "68", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "69", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "70", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "71", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "72", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "73", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "74", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "75", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "76", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "77", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "78", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "79", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "80", bgcolor: "cyan", measure: useMeasure() }, 
+    // { key: "81", bgcolor: "yellow", measure: useMeasure() }, 
+    // { key: "82", bgcolor: "magenta", measure: useMeasure() }, 
+    // { key: "83", bgcolor: "cyan", measure: useMeasure() }, 
+    // { key: "84", bgcolor: "yellow", measure: useMeasure() }, 
+    // { key: "85", bgcolor: "magenta", measure: useMeasure() }, 
+    // { key: "86", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "87", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "88", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "89", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "90", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "91", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "92", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "93", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "94", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "95", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "96", bgcolor: "yellow", measure: useMeasure() },
+    // { key: "97", bgcolor: "magenta", measure: useMeasure() },
+    // { key: "98", bgcolor: "cyan", measure: useMeasure() },
+    // { key: "99", bgcolor: "yellow", measure: useMeasure() },
   ]
   
-  const gridItems = useRef([])
-  const oldPositions = useRef(Array.from(elements).fill({}))
   const newPositions = useRef(Array.from(elements).fill({}))
-
-  const updatePosition = (idx, topRaw, leftRaw, top, left) => {
-    if (idx === 0){
-      oldPositions.current[idx] = {topRaw, leftRaw, top, left}
-    }else if (Object.keys(oldPositions.current[idx-1]).length){
-      oldPositions.current[idx] = {topRaw, leftRaw, top, left}
-    }
-  }
 
   const getItemWidth = (idx) => {
     if (elements[idx])
@@ -119,11 +150,12 @@ export default function App() {
   }
 
   const calculateLayout = (elements, marginTop, marginRight, marginBottom, marginLeft, containerWidth) => {
-    // FIXME what if item does not fit!? 
+    
     let currentRow = 0
     let currentTopOffset = 0
     let currentLeftOffset = 0
     let spaceRemainingX = containerWidth
+    
     const nextRow = () => {
       currentRow += 1
       currentTopOffset += defaultItemHeight // TODO adjust to variable height
@@ -133,7 +165,7 @@ export default function App() {
     
     elements.forEach((e, i) => {
       const necessarySpaceX = marginLeft + getItemWidth(i) + marginRight
-      while (spaceRemainingX <= necessarySpaceX){
+      while (spaceRemainingX <= necessarySpaceX && containerWidth > getItemWidth(i)){
         nextRow()
       }
       // console.log(`updating ${i}: row - ${currentRow}, top - ${currentTopOffset + marginTop}, left - ${currentLeftOffset + marginLeft}`)
@@ -145,189 +177,26 @@ export default function App() {
     })
   }
   
-  useEffect(() => {
+  const gridItems = useMemo(() => {
     calculateLayout(elements, 0, 0, 20, 0, contentWidth)
-  },[...elements])
-
-  useEffect(() => {
-    const getItemWidth = (idx) => {
-      if (elements[idx])
-        return elements[idx].measure[1].width
-      return 0
-    }
-
-    const getItemsWidth = (from, to) => 
-      from !== to 
-        ? elements
-        .slice(from, to)
-        .reduce((acc, el) => acc + el.measure[1].width, 0)
-        : 0
-
-    const getPreviousItemsWidth = (idx) => getItemsWidth(0, idx)
-    
-    const getLeftOffsetRaw = (idx) => getPreviousItemsWidth(idx);
-    
-    const getTopOffsetRaw = (idx, containerWidth) => {
-      const leftRaw = getLeftOffsetRaw(idx)
-      const itemWidth = getItemWidth(idx)
-      
-      const offsetRaw = (leftRaw + itemWidth)/containerWidth
-      const offset = Math.floor(offsetRaw)
-      
-      return offset
-    }
-
-    const getAdjustedTopOffsetRaw = (idx) => oldPositions.current[idx].topRaw
-
-    const getPreviousItemsInRow = (idx, containerWidth) => {
-      let currentItem = idx-1
-      let elementsInRow = []
-      const adjustedTopOffsetRaw = getAdjustedTopOffsetRaw(idx)
-      // console.log(`filtering out in getPreviousItemsInRow(${idx}, ${containerWidth}) - `)
-      // console.log(`positions.current:`)
-      // console.log(positions.current)
-      oldPositions.current
-        .slice(0,idx)
-        .forEach(({topRaw}, i) => {if (topRaw === adjustedTopOffsetRaw) elementsInRow.push(i)})
-      // console.log(`returning:`)
-      // console.log(elementsInRow)
-      return elementsInRow
-      // let currentOffset = getTopOffsetRaw(currentItem, containerWidth)
-      // while(currentItem >= 0 && currentOffset == topOffsetRaw){
-      //   elementsInRow.unshift(currentItem)
-      //   // console.log(`currentOffset: ${currentOffset}, currentItem: ${currentItem}, elementsInRow: ${elementsInRow}`)
-      //   currentItem = currentItem -1
-      //   currentOffset = getTopOffsetRaw(currentItem, containerWidth)
-      // }
-      // console.log(`getPreviousItemsInRow(${idx}, ${containerWidth}) = ${elementsInRow}`)
-      // return elementsInRow
-    }
-
-    const getPreviousItemsWidthForRow = (idx, containerWidth) => {
-      const previousItems = getPreviousItemsInRow(idx, containerWidth)
-      // console.log(previousItems)
-      const firstItemInRow = previousItems.length ? previousItems[0] : idx
-      const width = getItemsWidth(firstItemInRow, idx)
-      // console.log(`getItemsWidth(${firstItemInRow}, ${idx}) = ${width}`)
-      return width
-    }
-
-    const calculateTopPx = (
-      idx,
-      itemHeight,
-      marginY,
-      containerWidth,
-      print = false
-    ) => {
-      if (!containerWidth) return [0,0]
-
-      const leftOffsetRaw = getLeftOffsetRaw(idx);
-      let topOffsetRaw = getTopOffsetRaw(idx, containerWidth);
-
-      const piwfr = getPreviousItemsWidthForRow(idx, containerWidth)
-      const iw = getItemWidth(idx)
-
-      if (containerWidth < (piwfr + iw)){
-        topOffsetRaw++
-      }
-
-      const adjustedTopOffset = topOffsetRaw
-        ? topOffsetRaw * (itemHeight + marginY)
-        : 0
-
-      if (print){
-        console.log(`%ccalculateTopPx - idx = ${idx}, element: ${elements[idx].key}`, 'font-weight:bold');
-        console.log(`getPreviousItemsWidthForRow(${idx}, ${containerWidth}) = ${piwfr}`)
-        // console.log(`previousItemsWidth = ${getPreviousItemsWidth(idx)}`);
-        // console.log(`leftOffsetRaw = ${leftOffsetRaw}`);
-        // console.log(`topOffsetRaw = ${topOffsetRaw}`);
-        // console.log(`containerWidth = ${containerWidth}`);
-      }
-      
-      return [topOffsetRaw, adjustedTopOffset];
-    };
-
-    const calculateLeftPx = (
-      idx,
-      containerWidth,
-      print = false
-    ) => {
-      if (!containerWidth) return [0,0]
-
-      if (print) console.log(`%ccalculateLeftPx - idx = ${idx}, element: ${elements[idx].key}`, 'font-weight:bold');
-
-
-      const leftOffsetRaw = getLeftOffsetRaw(idx);
-      // const topOffsetRaw = getTopOffsetRaw(idx,  containerWidth)
-      const adjustedTopOffsetRaw = getAdjustedTopOffsetRaw(idx)
-
-      // in how many rows of length (containerWidth - marginX) can I fit
-      // all the previous boxes
-      let piwfr = 
-        getPreviousItemsWidthForRow(idx,  containerWidth)
-      let adjustedLeftOffset = piwfr
-
-      const iw = getItemWidth(idx)
-
-      // if (containerwidth < (adjustedLeftOffset + iw)){
-      //   left
-      // }
-        
-
-      // const adjustedLeftOffset = (idx % topOffset) * leftOffsetRaw;
-      if (print) {
-        // console.log(`itemWidth = ${getItemWidth(idx)}`);
-        // console.log(`leftOffsetRaw = ${leftOffsetRaw}`);
-        console.log(`adjustedTopOffsetRaw = ${adjustedTopOffsetRaw}`);
-        // console.log(`previousItemsWidth = ${getPreviousItemsWidth(idx)}`);
-        console.log(`previousItemsWidthForRow = ${piwfr}`);
-        // // console.log(`previousItemsInRow = ${getPreviousItemsInRow(idx, containerWidth)}`);
-        // console.log(`adjustedLeftOffset = ${adjustedLeftOffset}`);
-        // console.log(`containerWidth = ${containerWidth}`);
-      }
-      // FIXME -> return just one adjustedOffset
-      // adjustedLeftOffset = (adjustedLeftOffset + getItemWidth(idx)) < containerWidth
-      //   ? leftOffsetRaw % containerWidth
-      //   : adjustedLeftOffset;
-
-      return [leftOffsetRaw, adjustedLeftOffset];
-    };
     
     let gridItemsCalcs = elements.map((el, i) => {
-      // const [topRaw, top] = 
-      //   calculateTopPx(
-      //     i,
-      //     defaultItemHeight,
-      //     defaultMarginY,
-      //     gridSize.width,
-      //     // true
-      //   );
-      // const [leftRaw, left] =
-      //   calculateLeftPx(
-      //     i,
-      //     gridSize.width,
-      //     // true
-      //   );
-      // console.log(`containerWidth: ${gridSize.width}`)
+
       const xy = [
         newPositions.current[i].top,
         newPositions.current[i].left
       ]
-      // updatePosition(i, 
-      //   topRaw,
-      //   leftRaw,// getLeftOffsetRaw(i),
-      //   top, left)
+
       const w = clicked.includes(el.key) ? 50 : 20
-      // console.log({...el, xy})
       return {...el, xy, w}
     })
     // console.log(`passing through useMemo`)
-    gridItems.current = gridItemsCalcs
+    return gridItemsCalcs
   }, [gridSize.width, elements, clicked])
 
 
 
-  const transitions = useTransition(gridItems.current, el => el.key, {
+  const transitions = useTransition(gridItems, el => el.key, {
     from: ({xy, w}) => ({xy, w, opacity: 0}),
     enter: ({xy, w}) => ({xy, w, opacity: .5}),
     update: ({xy, w}) => ({xy, w}),
@@ -363,10 +232,6 @@ export default function App() {
                 }else{
                   setClicked([...clicked, item.key])
                 }
-                // const el = elements.find(e => e.key == item.key)
-                // elements[0].clicked = !elements[0].clicked
-                // // elements[elements.findIndex(e => e.key == item.key)].clicked = !el.clicked
-                // setAnyClicked(elements.some(e => e.clicked))
               }}
             >
               {item.key}
@@ -410,29 +275,6 @@ export default function App() {
                 </tbody>
             </table>
         </div>
-        <div>
-          Old calc
-            <table>
-                <thead>
-                  <th>el</th>
-                  <th>topRaw</th>
-                  <th>leftRaw</th>
-                  <th>top</th>
-                  <th>left</th>
-                </thead>
-                <tbody>
-                {oldPositions.current.map(({topRaw, leftRaw, top, left}, i) =>
-                  <tr>
-                    <td>{elements[i].key}</td>
-                    <td>{topRaw}</td>
-                    <td>{leftRaw}</td>
-                    <td>{top}</td>
-                    <td>{left}</td>
-                  </tr>
-                )}
-                </tbody>
-            </table>
-        </div> 
       </div>
     </div>
   );
