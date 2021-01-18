f59caf016d1615cf2e7d13d604d8d832fd2212e8
mikemajara <fmiguel05@gmail.com>
2021-01-18 18:44:12 +0100

    default margins to 0

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,7 +1,7 @@
-export const defaultMarginTop = 10
-export const defaultMarginRight = 10
-export const defaultMarginBottom = 10
-export const defaultMarginLeft = 10
+export const defaultMarginTop = 0
+export const defaultMarginRight = 0
+export const defaultMarginBottom = 0
+export const defaultMarginLeft = 0
 
 export const defaultItemHeight = 40;
 export const defaultItemWidth = 40;
